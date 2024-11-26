c11abcff48770062fdb201704c04b4b15c7cf325
ludoRap33 <ludovicdufaure@gmail.com>
2024-11-26 14:33:57 +0100

    five step

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -51,11 +51,11 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
+      <section id="nos-realisations" className="EventsContainer">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
+      <section id="notre-equipe" className="PeoplesContainer">
         <h2 className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
